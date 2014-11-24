aac1fad84e8f332af01076fea1ed9c37cba505dd
Pedram Pejman <pedrampejman2010@gmail.com>
2014-11-23 23:25:22 -0500

    added comments, some functionality

--- app/src/main/java/com/pedapps/cs2110example/GameActivity.java
+++ app/src/main/java/com/pedapps/cs2110example/GameActivity.java
@@ -81,7 +81,6 @@ public class GameActivity extends Activity implements View.OnTouchListener{
                 publishProgress();
                 Log.d(X_MIN + " : " + X_MAX, Y_MIN + " : " + Y_MAX);
                 Log.d("x,y", image.getX() + ", " + image.getY());
-
             }
             return null;
         }
@@ -104,17 +103,26 @@ public class GameActivity extends Activity implements View.OnTouchListener{
             if (image.getX() < X_MIN ) {
                 vx *= -1;
             }
-            if (image.getY() < Y_MIN) {
-                if (detectCollisionWithPaddle())  vy *= -1;
+            if (image.getY() < Y_MIN) {     //The ball is touching the bottom of the screen
+                if (detectCollisionWithPaddle()) {
+                    vy *= -1;
+                }
+                else {
+                    //the GameActivity.this is a subtle trick. It's not a big deal if you don't understand it.
+                    //Essentially, we need to get ahold of an instance of the GameActivity class because it has the
+                    //Context that the Toast object requires. Again, Context is a weird class; don't worry if you don't fully get it.
+                    Toast.makeText(GameActivity.this, "You lost", Toast.LENGTH_SHORT).show();
+                    gameOn = false;
+                }
             }
             image.setX(image.getX() + vx);
             image.setY(image.getY() + vy);
         }
 
         public boolean detectCollisionWithPaddle() {
-            Log.d("paddle:", paddle.getX() + " : " + paddle.getY());
-            Log.d("ball:", image.getX() + " : " + image.getY());
-            return true;
+            //Log.d("paddle:", paddle.getX() + " : " + paddle.getY());
+            //Log.d("ball:", image.getX() + " : " + image.getY());
+            return ( (image.getX() > paddle.getX()) && (image.getX() < (paddle.getX() + paddle.getMeasuredWidth())));
         }
     }
 }
